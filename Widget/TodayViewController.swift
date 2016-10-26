372cb9d357e2390b087bf01f0b3de5ca9de454ca
Dan Lindsay <dlindsay72@hotmail.com>
2016-10-26 13:36:21 -0400

    Basic UI for Today Extension done

---
+++ Widget/TodayViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TodayViewController.swift
+//  Widget
+//
+//  Created by Dan Lindsay on 2016-10-26.
+//  Copyright © 2016 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: ((NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
